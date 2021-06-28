6b9df2847b6d851a0011ecb92e027262b8bcd903
Jorge Ramos <jorge.ardcs@gmail.com>
2021-06-28 20:24:20 -0300

    Used small letters at the beginning of "array"

--- create2dArray.test.js
+++ create2dArray.test.js
@@ -1,9 +1,9 @@
 const create2dArray = require('./create2dArray')
 
-test('It creates an Array',() => {
+test('It creates an array',() => {
     expect(create2dArray()).toBeInstanceOf(Array);
 });
 
-test('It creates an Array of Arrays',() => {
+test('It creates an array of arrays',() => {
     expect(create2dArray()[0]).toBeInstanceOf(Array);
 });

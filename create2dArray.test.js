863bb8a06c08d6039d5de5e8248b0b33ca8d7102
Jorge Ramos <jorge.ardcs@gmail.com>
2021-06-28 11:56:13 -0300

    implemented tests for array and 2d array creation

--- create2dArray.test.js
+++ create2dArray.test.js
@@ -1,1 +1,9 @@
 const create2dArray = require('./create2dArray')
+
+test('It creates an Array',() => {
+    expect(create2dArray()).toBeInstanceOf(Array);
+});
+
+test('It creates an Array of Arrays',() => {
+    expect(create2dArray()[0]).toBeInstanceOf(Array);
+});

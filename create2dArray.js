25ae657f38bb0022078b68e84f585daf1379d448
Jorge Ramos <jorge.ardcs@gmail.com>
2021-06-28 23:19:13 -0300

    Did some nasty ass refactoring yo I mean look at that

--- create2dArray.js
+++ create2dArray.js
@@ -1,14 +1,7 @@
 function create2dArray(n, m){
     const n_row = (n === undefined ? Math.floor(Math.random() * 10) + 1 : n);
     const n_column = (m === undefined ? Math.floor(Math.random() * 10) + 1 : m);
-    const grid = [];
-    for (let i = 0; i < n_row; i++){
-        grid.push([])
-        for (let j = 0; j < n_column; j++){
-            grid[i].push(0);
-        }
-    }
-
+    const grid = Array(n_row).fill(Array(n_column));
 
     return grid;
 }

6a402c944346a2a6d424dc24774a6d4b14e4ad8e
Jorge Ramos <jorge.ardcs@gmail.com>
2021-06-29 12:19:16 -0300

    Improved function as to map true random integers to each of the columns' elements

--- create2dArray.js
+++ create2dArray.js
@@ -1,8 +1,10 @@
 function create2dArray(n, m){
     const n_row = (n === undefined ? Math.floor(Math.random() * 10) + 1 : n);
     const n_column = (m === undefined ? Math.floor(Math.random() * 10) + 1 : m);
-    const grid = Array(n_row).fill(Array(n_column));
-
+    const grid = new Array(n_row);
+    for (let i = 0; i < grid.length; i++){
+        grid[i] = [...Array(n_column)].map(() => {return Math.round(Math.random())});
+    }
     return grid;
 }
 

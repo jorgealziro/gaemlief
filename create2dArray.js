e7e9e983e6ae8ac5186e13fae9d61bab3ca6e7ca
jorgealziro <jorge.ardcs@gmail.com>
2021-07-01 00:37:54 -0300

    defined lower limit to undefined number of rows and columns to 10

--- create2dArray.js
+++ create2dArray.js
@@ -1,6 +1,6 @@
 function create2dArray(n, m){
-    const n_row = (n === undefined ? Math.floor(Math.random() * 10) + 1 : n);
-    const n_column = (m === undefined ? Math.floor(Math.random() * 10) + 1 : m);
+    const n_row = (n === undefined ? Math.floor(Math.random() * 10) + 10 : n);
+    const n_column = (m === undefined ? Math.floor(Math.random() * 10) + 10 : m);
     const grid = new Array(n_row);
     for (let i = 0; i < grid.length; i++){
         grid[i] = [...Array(n_column)].map(() => {return Math.round(Math.random())});

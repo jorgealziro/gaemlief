bc5044ca1cb254bdf971554eb9ea6b8b0583cc15
jorgealziro <jorge.ardcs@gmail.com>
2021-07-01 00:39:11 -0300

    Added exception if user tries to define a number of rows or columns of less than 10

--- create2dArray.js
+++ create2dArray.js
@@ -1,6 +1,9 @@
 function create2dArray(n, m){
     const n_row = (n === undefined ? Math.floor(Math.random() * 10) + 10 : n);
     const n_column = (m === undefined ? Math.floor(Math.random() * 10) + 10 : m);
+    if (n_row < 10 || n_column < 10){
+        throw new Error('Grid must be of at least 10x10');
+    }
     const grid = new Array(n_row);
     for (let i = 0; i < grid.length; i++){
         grid[i] = [...Array(n_column)].map(() => {return Math.round(Math.random())});

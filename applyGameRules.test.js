98712f601e7ea41fa31025e234f6bbcaf038fa15
jorgealziro <jorge.ardcs@gmail.com>
2021-07-07 17:19:15 -0300

    Made changes on previous test because business rules changed also yolo

--- applyGameRules.test.js
+++ applyGameRules.test.js
@@ -2,8 +2,12 @@ const applyGameRules = require('./applyGameRules');
 const create2DArray = require('./create2dArray');
 
 test('It ensures sum of neighbor cells is a number',() => {
-    const grid = create2DArray();
-    for (column of grid){
+    const grid = [[Math.round(Math.random()),Math.round(Math.random()),Math.round(Math.random())],
+    [Math.round(Math.random()),Math.round(Math.random()),Math.round(Math.random())],
+    [Math.round(Math.random()),Math.round(Math.random()),Math.round(Math.random())]];
+    
+    expect(applyGameRules.checkNeighborSum(grid)).toEqual(expect.any(Number));
+})
         for (cell of column){
             expect(applyGameRules.checkNeighborSum(cell)).toBe(expect.any(number));
         }

6bee9999c2f03a27fe82db581b143f63468a7ece
jorgealziro <jorge.ardcs@gmail.com>
2021-07-07 17:22:25 -0300

    Performed test to neighbor summer function

--- applyGameRules.test.js
+++ applyGameRules.test.js
@@ -8,8 +8,23 @@ test('It ensures sum of neighbor cells is a number',() => {
     
     expect(applyGameRules.checkNeighborSum(grid)).toEqual(expect.any(Number));
 })
+
+test('It sums neighbor cells of the inner cell of a 3x3 grid',() => {
+    const grid = [[Math.round(Math.random()),Math.round(Math.random()),Math.round(Math.random())],
+    [Math.round(Math.random()),Math.round(Math.random()),Math.round(Math.random())],
+    [Math.round(Math.random()),Math.round(Math.random()),Math.round(Math.random())]];
+    const innerCell = grid[1][1];
+
+    function sum2DArr(grid3x3){
+        let sum = 0;
+        for (column of grid3x3){
         for (cell of column){
-            expect(applyGameRules.checkNeighborSum(cell)).toBe(expect.any(number));
+                sum += cell;
         }
     }
+ 
+        return sum;
+    }
+
+    expect(applyGameRules.checkNeighborSum(grid)).toEqual(sum2DArr(grid) - innerCell);
 })
